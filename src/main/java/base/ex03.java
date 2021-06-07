ac4650b9ff1b1522cb736ddaf0ae107af1e26d33
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 20:57:16 -0400

    Update ex03.java

--- src/main/java/base/ex03.java
+++ src/main/java/base/ex03.java
@@ -1,3 +1,7 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Korinne Ramcharitar
+ */
 package base;
 //Quotation marks are often used to denote the start and end of a string. But sometimes we need to print out the quotation marks themselves by using escape characters.
 //Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output
